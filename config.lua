ce0343dfad79220e7db58b1d5c484b2a063a5662
KnetterbalScripts <mathiasborgers1994@gmail.com>
2025-08-21 04:21:42 +0200

    upload

--- config.lua
+++ config.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 SM:AddTab("Skill")
 SM:Dropdown("Skill:" ,"selectedSkill",{"FLETCHING","COOKING","FIREMAKING"}, "FLETCHING")
 
@@ -16,22 +15,3 @@ SM:Dropdown("Fish Type", "fishType", {"NONE","SHRIMP", "ANCHOVIES", "TROUT", "SA
 SM:AddTab("Firemaking")
 SM:Dropdown("Log Type", "logType", {"NONE","NORMAL", "OAK", "WILLOW", "MAPLE", "YEW", "MAGIC"}, "NONE")
 
-=======
-SM:AddTab("Skill")
-SM:Dropdown("Skill:" ,"selectedSkill",{"FLETCHING","COOKING","FIREMAKING"}, "FLETCHING")
-
-SM:AddTab("Fletching")
-SM:Dropdown("Sub Skill", "subSkill", {"NONE","FLETCH", "STRING", "HEADLESS", "ARROWS"}, "NONE")
-SM:Dropdown("Log Type", "logType", {"NONE","NORMAL", "OAK", "WILLOW", "MAPLE", "YEW", "MAGIC"}, "NONE")
-SM:Dropdown("Arrowhead Type", "arrowheadType", {"NONE","BRONZE", "IRON", "STEEL", "MITHRIL", "ADAMANT", "RUNE", "DRAGON", "BROAD"}, "NONE")
-SM:Dropdown("Bow Type", "bowType", {"NONE","SHORTBOW", "LONGBOW"}, "NONE")
-SM:Dropdown("unf shortbows", "bowMaterial1", {"NONE","NORMAL", "OAK", "WILLOW", "MAPLE", "YEW", "MAGIC"}, "NONE")
-SM:Dropdown("unf longbows", "bowMaterial2", {"NONE","NORMAL", "OAK", "WILLOW", "MAPLE", "YEW", "MAGIC"}, "NONE")
-
-SM:AddTab("Cooking")
-SM:Dropdown("Fish Type", "fishType", {"NONE","SHRIMP", "ANCHOVIES", "TROUT", "SALMON", "TUNA", "SWORDFISH", "LOBSTER", "BASS", "SHARK", "MANTA_RAY", "KARAMBWAN", "KARAMBWANJI", "CAVEFISH", "ROCKTAIL", "SAILFISH"}, "NONE")
-
-SM:AddTab("Firemaking")
-SM:Dropdown("Log Type", "logType", {"NONE","NORMAL", "OAK", "WILLOW", "MAPLE", "YEW", "MAGIC"}, "NONE")
-
->>>>>>> 3714bb59b834c62544d3d7b949349b2698273340
